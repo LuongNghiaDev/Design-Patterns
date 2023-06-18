e582f79def12915c1dd4f978f92467f7d3053e1a
rick <rickcool0111@gmail.com>
2023-06-18 11:39:24 +0700

    push Singleton

--- Demo/Demo/CreationalPatterns/SingletonDesignPattern/README.md
+++ Demo/Demo/CreationalPatterns/SingletonDesignPattern/README.md
@@ -1,4 +1,9 @@
 # Design-Patterns
-Strategy Design Pattern
+Singleton Design Pattern
 
-- Áp dụng nguyên tắc thứ 2 của SOLID open-closed principle
+- Đảm bảo một lớp chỉ có một phiên bản và cung cấp một điểm truy cập toàn cầu cho nó.
+
+- Gói gọn "khởi tạo đúng lúc" hoặc "khởi tạo trong lần sử dụng đầu tiên"
+
+=> Lưu ý: Về vấn đề đa luồng , chạy trên các thread khác nhau cùng 1 lần vẫn sẽ gây ra vấn đề tạo ra nhiều các instance khác nhau (tức chạy trên 2 thread Singleton sẽ tạo ra 2 instance khác nhau) đó là hạn chế của singleton
+=> Xem code để chống lỗi multi threading
---
+++ Demo/Demo/CreationalPatterns/SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.CreationalPatterns.SingletonDesignPattern
+{
+    public class Singleton
+    {
+
+        private static volatile Singleton uniqueSingleton;
+        private static readonly object lockObject = new object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            //lazy
+            if (uniqueSingleton == null)
+            {
+                lock (lockObject)
+                {
+                    if (uniqueSingleton == null)
+                    {
+                        uniqueSingleton = new Singleton();
+                    }
+                }
+            }
+            return uniqueSingleton;
+        }
+    }
+}
